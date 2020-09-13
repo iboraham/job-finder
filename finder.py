9ac549e031cd2207cdbf5c20337883e7119610a8
Onur Şerbetçi <data-science-home@users.noreply.github.com>
2020-09-14 00:02:24 +0300

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,7 +1,6 @@
 from indeed_job_scraper import Scraper
 
 if __name__ == '__main__':
-    # search_terms_input = input("Please enter a keywords separated by commas:\n")
     search_terms_input = 'turkish'
     print("input verified, input is:" + str(search_terms_input))
     sc = Scraper(search_terms_input,
