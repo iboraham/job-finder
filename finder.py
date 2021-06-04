ccc066d3748a2def521d04ab8d1ff8f36de631be
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-06-05 00:09:35 +0100

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,10 +1,29 @@
 from indeed_job_scraper import Scraper
+import pandas as pd
+import argparse
+from cv import CV
 
-if __name__ == '__main__':
-    search_terms_input = 'turkish'
-    print("input verified, input is:" + str(search_terms_input))
-    sc = Scraper(search_terms_input,
-                 start_invisible=True)
+
+def main(args):
+    search_terms_input = str(args.search)
+    cv_url = str(args.cv_url)
+
+    # Scrap indeed.co.uk
+    sc = Scraper(search_terms_input, start_invisible=True)
     sc.scrap_data()
     df = sc.df.copy()
-    df.to_csv('deneme.csv')
+
+    # Read CV
+    cv = CV(cv_url)
+    cv.read_cv()
+
+    # NLP
+    # Need update here... (word2vec -> similarity calculation)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search", required=True)
+    parser.add_argument("-c", "--cv-url", required=True)
+    args = parser.parse_args()
+    main(args)
