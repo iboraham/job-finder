41cb45d573160346665ed88ad3638b50f0a0da3d
Onur Şerbetçi <onurserbetcii@gmail.com>
2021-06-05 22:14:49 +0100

    Update __init__.py

--- cv/__init__.py
+++ cv/__init__.py
@@ -5,20 +5,11 @@ import urllib
 class CV:
     def __init__(self, url):
         self.url = url
-
-    def read_cv(self):
-
-        # creating a pdf file object
         pdfFileObj = urllib.request.urlopen(self.url)
-
-        # creating a pdf reader object
         pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-        # creating a page object
         pageObj = pdfReader.getPage(0)
-
-        # extracting text from page
         self.text = pageObj.extractText()
-
-        # closing the pdf file object
         pdfFileObj.close()
+
+    def get_cv(self):
+        return self.text
